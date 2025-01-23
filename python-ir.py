1834c66baf288270137e691901a8906a1853c3e5
Christophe <christophe@prettysimplegames.com>
2025-01-23 11:23:11 +0100

    Add thread and queue

--- python-ir.py
+++ python-ir.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import pigpio
 import time
@@ -18,6 +18,7 @@ PORT = 8080
 GPIO = 18
 FREQ = 38.0
 
+
 def log_verbose(msg: str) -> str:
     if VERBOSE:
         print(msg)
@@ -29,12 +30,112 @@ def log_info(msg: str) -> str:
     return msg
 
 
+class IRtx(threading.Thread):
+    def __init__(self, gpio, freq):
+        super().__init__()
+        self._gpio = gpio
+        self._freq = freq
+
+        self._pi = None
+        self._todo = []
+        self._continue = True
+
+    def run(self):
+        while self._continue:
+            if self._todo:
+                log_verbose("Connect to pi")
+                self._pi = pigpio.pi()  # Connect to Pi.
+                self._pi.set_mode(self._gpio, pigpio.OUTPUT)  # IR TX connected to this gpio.
+
+                # play all queued codes
+                while self._todo:
+                    (key, code) = self._todo.pop()
+                    IRtx.send_code(self._pi, key, code, self._gpio, self._freq)
+
+                log_verbose("Disconnect from Pi.")
+                self._pi.stop()
+                self._pi = None
+
+            time.sleep(0.05)
+
+    def queue_code(self, key, code):
+        self._todo.append((key, code))
+
+    def stop(self):
+        self._continue = False
+
+    @staticmethod
+    def send_code(pi, key, code, gpio, freq):
+        log_verbose(f"Playing '{key}'")
+        pi.wave_add_new()
+
+        # Check marks
+        marks = {}
+        for i in range(0, len(code), 2):
+            if code[i] not in marks:
+                marks[code[i]] = -1
+
+        for i in marks:
+            wf = IRtx.carrier(gpio, freq, i)
+            pi.wave_add_generic(wf)
+            wid = pi.wave_create()
+            marks[i] = wid
+
+        # Check spaces
+        spaces = {}
+        for i in range(1, len(code), 2):
+            if code[i] not in spaces:
+                spaces[code[i]] = -1
+
+        for i in spaces:
+            pi.wave_add_generic([pigpio.pulse(0, 0, i)])
+            wid = pi.wave_create()
+            spaces[i] = wid
+
+        # Create wave
+        wave = [0] * len(code)
+        for i in range(0, len(code)):
+            if i & 1:  # Space
+                wave[i] = spaces[code[i]]
+            else:  # Mark
+                wave[i] = marks[code[i]]
+
+        pi.wave_chain(wave)
+
+        while pi.wave_tx_busy():
+            time.sleep(0.05)
+
+        for i in marks:
+            pi.wave_delete(marks[i])
+        for i in spaces:
+            pi.wave_delete(spaces[i])
+
+    @staticmethod
+    def carrier(gpio, frequency, micros, dutycycle=0.5):
+        """
+        Generate cycles of carrier on gpio with frequency and dutycycle.
+        """
+        wf = []
+        cycle = 1000.0 / frequency
+        cycles = int(round(micros / cycle))
+        on = int(round(cycle * dutycycle))
+        sofar = 0
+        for c in range(cycles):
+            target = int(round((c + 1) * cycle))
+            sofar += on
+            off = target - sofar
+            sofar += off
+            wf.append(pigpio.pulse(1 << gpio, 0, on))
+            wf.append(pigpio.pulse(0, 1 << gpio, off))
+        return wf
+
+
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        global irtx
 
         log_verbose(f"Received '{self.path}'")
         param = self.path.split("?")
-        msg = ''
         if len(param) == 2 and param[0] == '/switch':
             log_verbose(f"Param '{param[1]}'")
             param = param[1].split("=")
@@ -43,7 +144,7 @@ class MyServer(BaseHTTPRequestHandler):
                 log_verbose(f"Switch id 'key'")
                 if key in ROBLIN_IR:
                     msg = log_info(f"Switching <b>'{key}'</b>")
-                    send_code(ROBLIN_IR[key], GPIO, FREQ)
+                    irtx.queue_code(key, ROBLIN_IR[key])
                 else:
                     msg = log_info(f"Unknown key <b>'{key}'</b>")
             else:
@@ -59,81 +160,21 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes(f"<p>{msg}</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
-def carrier(gpio, frequency, micros, dutycycle=0.5):
-    """
-    Generate cycles of carrier on gpio with frequency and dutycycle.
-    """
-    wf = []
-    cycle = 1000.0 / frequency
-    cycles = int(round(micros / cycle))
-    on = int(round(cycle * dutycycle))
-    sofar = 0
-    for c in range(cycles):
-        target = int(round((c + 1) * cycle))
-        sofar += on
-        off = target - sofar
-        sofar += off
-        wf.append(pigpio.pulse(1 << gpio, 0, on))
-        wf.append(pigpio.pulse(0, 1 << gpio, off))
-    return wf
-
-def send_code(code, GPIO, FREQ):
-    pi = pigpio.pi()  # Connect to Pi.
-    pi.set_mode(GPIO, pigpio.OUTPUT)  # IR TX connected to this GPIO.
-
-    pi.wave_add_new()
-
-    print("Playing")
-
-    # Check marks
-    marks = {}
-    for i in range(0, len(code), 2):
-        if code[i] not in marks:
-            marks[code[i]] = -1
-
-    for i in marks:
-        wf = carrier(GPIO, FREQ, i)
-        pi.wave_add_generic(wf)
-        wid = pi.wave_create()
-        marks[i] = wid
-
-    # Check spaces
-    spaces = {}
-    for i in range(1, len(code), 2):
-        if code[i] not in spaces:
-            spaces[code[i]] = -1
-
-    for i in spaces:
-        pi.wave_add_generic([pigpio.pulse(0, 0, i)])
-        wid = pi.wave_create()
-        spaces[i] = wid
-
-    # Create wave
-    wave = [0] * len(code)
-    for i in range(0, len(code)):
-        if i & 1:  # Space
-            wave[i] = spaces[code[i]]
-        else:  # Mark
-            wave[i] = marks[code[i]]
-
-    pi.wave_chain(wave)
-
-    while pi.wave_tx_busy():
-        time.sleep(0.05)
-
-    for i in marks:
-        pi.wave_delete(marks[i])
-    for i in spaces:
-        pi.wave_delete(spaces[i])
 
 if __name__ == "__main__":
+    # Start the IR TX thread
+    irtx = IRtx(GPIO, FREQ)
+    irtx.start()
+
     webServer = HTTPServer((HOSTNAME, PORT), MyServer)
-    print("Server started http://%s:%s" % (HOSTNAME, PORT))
+    log_info("Server started http://%s:%s" % (HOSTNAME, PORT))
 
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
-
     webServer.server_close()
-    print("Server stopped.")
+
+    irtx.stop()
+    irtx.join()
+    log_verbose("Server stopped.")
